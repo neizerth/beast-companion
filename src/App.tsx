11798d8100698b2b44a0412ac28d6d12adf2bbed
Vladimir Yazykov <neizerth@gmail.com>
2023-04-18 16:08:50 +0300

    base

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react'
+import 'normalize.css/normalize.css';
+import S from './App.module.css'
+import {MapType} from "./util/interfaces";
+import {MapSelector} from "./components/organisms/MapSelector/MapSelector";
+import {GameMap} from "./components/organisms/GameMap/GameMap";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [mapType, setMap] = useState(MapType.NONE);
+  const onMapSelect = (type: MapType) => {
+      setMap(type);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className={S.app}>
+        {mapType === MapType.NONE ?
+            <MapSelector onSelect={onMapSelect}/> :
+            <GameMap type={mapType}/>}
     </div>
   )
 }
