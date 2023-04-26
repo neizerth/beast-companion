da4a02057c4dc23535d0a76c6bb5d4de2211fedb
Vladimir Yazykov <neizerth@gmail.com>
2023-04-26 18:56:16 +0300

    optimized image preloading

--- src/hooks/useImageDownloadProgress.ts
+++ src/hooks/useImageDownloadProgress.ts
@@ -2,16 +2,17 @@ import {useEffect, useState} from "react";
 import axios from "axios";
 
 export const useImageDownloadProgress = (url: string) => {
-    const [currentProgress, setProgress] = useState(0);
+    const [progress, setProgress] = useState(0);
 
     useEffect(() => {
-        axios.get(url, {
-            onDownloadProgress: ({ progress= 0 }) => currentProgress < 100 && setProgress(progress * 99)
-        });
+
         const img = new Image;
-        img.onload = () => setProgress(100);
         img.src = url;
+        axios.get(url, {
+            onDownloadProgress: ({ progress= 0 }) => setProgress(progress * 100)
+        })
+            .then(() => setProgress(100));
     }, []);
 
-    return currentProgress
+    return progress
 }
