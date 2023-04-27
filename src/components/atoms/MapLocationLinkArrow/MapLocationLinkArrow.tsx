6b6857daa6399a52091ae30ec45c3d978723c4a9
Vladimir Yazykov <neizerth@gmail.com>
2023-04-27 15:57:44 +0300

    added tip to arrow

--- src/components/atoms/MapLocationLinkArrow/MapLocationLinkArrow.tsx
+++ src/components/atoms/MapLocationLinkArrow/MapLocationLinkArrow.tsx
@@ -88,14 +88,20 @@ export const MapLocationLinkArrow = (props: MapLocationLinkArrowProps) => {
     ].join(' ');
 
     return (
-        <g>
+        <>
             <path
                 fill={'#fff'}
                 transform={transform}
                 className={S.icon}
                 d="M7.563,6H0.436C0.04,6-0.112,5.499,0.088,5.222l3.545-4.977 c0.202-0.276,0.531-0.346,0.733-0.069l3.545,5.081C8.113,5.533,7.958,6,7.563,6z"
             />
-        </g>
+            <path
+                transform={transform}
+                className={S.tip}
+                fill="black"
+                d="M 4.936 0.991 C 5.333 0.991 2.511 1.019 3.077 1.019 L 3.64 0.24 C 3.842 -0.036 4.162 -0.106 4.364 0.171 L 4.936 0.991 Z"
+            />
+        </>
         // <path fill="#fff" d={path.join(' ')} className={S.icon}/>
     )
 }
