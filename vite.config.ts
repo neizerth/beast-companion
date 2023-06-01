94e0c8831ca8898204f891fcd31bc69ea17859d5
Vladimir Yazykov <neizerth@gmail.com>
2023-06-01 10:55:44 +0300

    added pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
+import manifest from './manifest';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
       react(),
-      VitePWA({ registerType: 'autoUpdate' })
+      VitePWA({
+          registerType: 'autoUpdate',
+          manifest
+      })
   ],
   base: '/beast-companion/',
   build: {
