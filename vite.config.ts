0ec8cbd7d62420db2856fb2da727805b0c5df345
Vladimir Yazykov <neizerth@gmail.com>
2023-06-02 11:23:58 +0300

    fixed manifest name bug

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-import manifest from './manifest';
+import manifest from './baseManifest';
 
 // https://vitejs.dev/config/
 export default defineConfig({
