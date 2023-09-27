c937af7d887d25925ded7f339c4116eb93749731
Vladimir Yazykov <neizerth@gmail.com>
2023-09-27 07:35:03 +0300

    updated mode switch

--- src/components/organisms/ModeSwitch/ModeSwitch.tsx
+++ src/components/organisms/ModeSwitch/ModeSwitch.tsx
@@ -1,6 +1,7 @@
 import classnames from "classnames";
 import changeIcon from "../../../../public/images/change.svg";
 import pathIcon from "../../../../public/images/path.svg";
+import meepleIcon from "../../../../public/images/meeple.svg";
 import {GameControl} from "../..";
 
 import S from "./ModeSwitch.module.scss";
@@ -13,8 +14,9 @@ export interface ModeSwitchProps {
 }
 
 const icons = {
-    [GameMode.PATH]: changeIcon,
-    [GameMode.LOCATIONS]: pathIcon,
+    [GameMode.PATH]: pathIcon,
+    [GameMode.LOCATIONS]: changeIcon,
+    [GameMode.MEEPLE]: meepleIcon,
     [GameMode.MOVEMENT]: null
 }
 
@@ -22,11 +24,22 @@ export const ModeSwitch = (props: ModeSwitchProps) => {
     const { className } = props;
     const gameMode = useAppSelector(selectMode);
     const dispatch = useAppDispatch();
-    const icon = icons[gameMode];
+
+    const modes = [GameMode.PATH, GameMode.LOCATIONS, GameMode.MEEPLE];
+    const modeIndex = modes.indexOf(gameMode);
+    const nextIndex = modeIndex === modes.length - 1 ? 0 : modeIndex + 1;
+
+    const nextMode = modes[nextIndex];
+    const icon = icons[nextMode];
+    console.log({
+        gameMode,
+        nextMode
+    })
 
     const toggleMode = () => {
-        const mode = gameMode === GameMode.PATH ? GameMode.LOCATIONS : GameMode.PATH;
-        dispatch(setGameMode(mode));
+
+        // const mode = gameMode === GameMode.PATH ? GameMode.LOCATIONS : GameMode.PATH;
+        dispatch(setGameMode(nextMode));
     }
 
     return (
